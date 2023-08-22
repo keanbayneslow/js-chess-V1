f43f0670990d3003b612bcde9366a22a3d2044ed
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-08-22 13:23:24 +1000

    adds pieces and positions on start board

---
+++ app.js
@@ -0,0 +1,19 @@
+const gameBoard = document.querySelector("#gameboard")
+
+const playerDisplay = document.querySelector("#player")
+
+const infoDisplay = document.querySelector("#info-display")
+
+const width = 8
+
+const startPieces = [
+rook, knight, bishop, queen, king, bishop, knight, rook, 
+pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn, 
+'', '', '', '', '', '', '', '',
+'', '', '', '', '', '', '', '',
+'', '', '', '', '', '', '', '',
+'', '', '', '', '', '', '', '',
+pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+rook, knight, bishop, king, queen, bishop, knight, rook
+]
+
