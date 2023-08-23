930d79f6c2318a3354a5c41714c4fa30b7810402
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-08-23 16:37:03 +1000

    creates gameboard and adds pieces

--- app.js
+++ app.js
@@ -7,13 +7,31 @@ const infoDisplay = document.querySelector("#info-display")
 const width = 8
 
 const startPieces = [
-rook, knight, bishop, queen, king, bishop, knight, rook, 
-pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn, 
-'', '', '', '', '', '', '', '',
-'', '', '', '', '', '', '', '',
-'', '', '', '', '', '', '', '',
-'', '', '', '', '', '', '', '',
-pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
-rook, knight, bishop, king, queen, bishop, knight, rook
+    rook, knight, bishop, queen, king, bishop, knight, rook,
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    '', '', '', '', '', '', '', '',
+    pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+    rook, knight, bishop, king, queen, bishop, knight, rook
 ]
 
+function createBoard() {
+    startPieces.forEach((startPiece, i) => {
+        const square = document.createElement('div')
+        square.classList.add('square')
+        square.innerHTML = startPiece
+        square.setAttribute('square-id', i)
+        const row = Math.floor((63 - i) / 8) + 1
+        if (row % 2 === 0) {
+            square.classList.add(i % 2 === 0 ? "beige" : "green")
+        } else {
+            square.classList.add(i % 2 === 0 ? "green" : "beige")
+        }
+
+        gameBoard.append(square)
+    })
+}
+
+createBoard()
